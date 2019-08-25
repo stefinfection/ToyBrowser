cbdf8723a1bfbf1022f30f6811a1bc4138da8f80
stefinfection <sjeangeo@gmail.com>
2019-08-24 18:07:49 -0600

    gtk install success and tiny window working

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 mod network;
+mod graphics;
 
 fn main() {
     let test_url: &str = "http://example.org/index.html";
     let url_pieces = network::parse_url(test_url, false);
     network::request(&url_pieces.0, &url_pieces.1, &url_pieces.2);
 
+    graphics::init_window();
 }
 
 
